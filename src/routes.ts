ff8141d88e7ee265ebd515be29456628ef77b595
Momna Rani <hadiraja637@gmail.com>
2026-08-23 16:02:19 +0500

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,4 @@
+import { createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import Quran from './pages/Quran';
 import Hadith from './pages/Hadith';
@@ -23,3 +24,17 @@ export const routes = [
   { path: '/blog', component: Blog },
   { path: '/sunnah-habits', component: SunnahHabits }
 ];
+
+export const router = createBrowserRouter([
+  { path: '/', Component: Home },
+  { path: '/quran', Component: Quran },
+  { path: '/hadith', Component: Hadith },
+  { path: '/duas', Component: Duas },
+  { path: '/qibla', Component: Qibla },
+  { path: '/calendar', Component: Calendar },
+  { path: '/zakat', Component: Zakat },
+  { path: '/tasbeeh', Component: Tasbeeh },
+  { path: '/stories', Component: Stories },
+  { path: '/blog', Component: Blog },
+  { path: '/sunnah-habits', Component: SunnahHabits }
+]);
