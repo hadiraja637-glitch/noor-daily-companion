d5ac39be3212366aeb3911fd55c638d4658a8080
Momna Rani <hadiraja637@gmail.com>
2026-08-23 15:44:43 +0500

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,3 @@
-import { createBrowserRouter } from 'react-router';
-import Layout from './components/Layout';
 import Home from './pages/Home';
 import Quran from './pages/Quran';
 import Hadith from './pages/Hadith';
@@ -7,36 +5,21 @@ import Duas from './pages/Duas';
 import Qibla from './pages/Qibla';
 import Calendar from './pages/Calendar';
 import Zakat from './pages/Zakat';
+import Tasbeeh from './pages/Tasbeeh';
 import Stories from './pages/Stories';
 import Blog from './pages/Blog';
-import InfoPage from './pages/InfoPage';
-import StoryDetail from './pages/StoryDetail';
-import Donate from './pages/Donate';
-import Journey from './pages/Journey';
-import Tasbeeh from './pages/Tasbeeh';
+import SunnahHabits from './pages/SunnahHabits';
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    Component: Layout,
-    children: [
-      { index: true, Component: Home },
-      { path: 'quran', Component: Quran },
-      { path: 'hadith', Component: Hadith },
-      { path: 'duas', Component: Duas },
-      { path: 'qibla', Component: Qibla },
-      { path: 'calendar', Component: Calendar },
-      { path: 'zakat', Component: Zakat },
-      { path: 'stories', Component: Stories },
-      { path: 'stories/:slug', Component: StoryDetail },
-      { path: 'blog', Component: Blog },
-      { path: 'donate', Component: Donate },
-      { path: 'journey', Component: Journey },
-      { path: 'tasbeeh', Component: Tasbeeh },
-      { path: 'privacy', Component: InfoPage },
-      { path: 'terms', Component: InfoPage },
-      { path: 'contact', Component: InfoPage },
-      { path: 'about', Component: InfoPage },
-    ],
-  },
-]);
+export const routes = [
+  { path: '/', component: Home },
+  { path: '/quran', component: Quran },
+  { path: '/hadith', component: Hadith },
+  { path: '/duas', component: Duas },
+  { path: '/qibla', component: Qibla },
+  { path: '/calendar', component: Calendar },
+  { path: '/zakat', component: Zakat },
+  { path: '/tasbeeh', component: Tasbeeh },
+  { path: '/stories', component: Stories },
+  { path: '/blog', component: Blog },
+  { path: '/sunnah-habits', component: SunnahHabits }
+];
