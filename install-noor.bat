87528289200f975e24f9e29c25c31d7c0d74eab1
Momna Rani <hadiraja637@gmail.com>
2026-08-28 22:12:24 +0500

    Delete install-noor.bat

--- install-noor.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Installing Noor website dependencies...
-echo.
-npm install
-echo.
-echo Installation finished or stopped.
-echo If there is an error, take a screenshot of this window.
-pause
