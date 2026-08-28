344bdf7f543fc4e580830e7612395412106cfada
Momna Rani <hadiraja637@gmail.com>
2026-08-28 18:41:00 +0500

    Update NoorLogo.tsx

--- src/components/NoorLogo.tsx
+++ src/components/NoorLogo.tsx
@@ -1,31 +1,43 @@
 import React from "react";
 
+type NoorLogoVariant = "navbar" | "footer";
+
 interface NoorLogoProps {
+  variant?: NoorLogoVariant;
   className?: string;
-  size?: number;
 }
 
 const NoorLogo: React.FC<NoorLogoProps> = ({
+  variant = "navbar",
   className = "",
-  size = 44,
 }) => {
+  const logoSrc =
+    "/images/ChatGPT%20Image%20Aug%2028%2C%202026%2C%2006_31_11%20PM.png";
+
+  const sizeClasses = {
+    navbar:
+      "h-14 sm:h-[58px] lg:h-[62px]",
+    footer:
+      "h-12 sm:h-[52px] lg:h-[56px]",
+  };
+
   return (
-    <div
-      className={`flex shrink-0 items-center justify-center overflow-hidden bg-transparent ${className}`}
-      style={{
-        height: `${size}px`,
-      }}
-    >
-      <img
-        src="/images/ChatGPT%20Image%20Aug%2028%2C%202026%2C%2006_31_11%20PM.png"
-        alt="NOOR - Islamic Daily Companion"
-        draggable={false}
-        className="block h-full w-auto max-w-full object-contain bg-transparent"
-        style={{
-          backgroundColor: "transparent",
-        }}
-      />
-    </div>
+    <img
+      src={logoSrc}
+      alt="Noor"
+      draggable={false}
+      className={[
+        "block",
+        "w-auto",
+        "max-w-none",
+        "object-contain",
+        "shrink-0",
+        "bg-transparent",
+        "select-none",
+        sizeClasses[variant],
+        className,
+      ].join(" ")}
+    />
   );
 };
 
